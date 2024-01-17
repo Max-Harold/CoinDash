92b08686fa0e20081ca14b3bb05caae455b288cc
Max Harold <maximilian.harold@gmail.com>
2024-01-16 21:19:09 -0600

    added a little extra

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,13 +1,11 @@
 extends Area2D
 
+signal pickup
+signal hurt
+
 @export var speed = 350
 var velocity = Vector2.ZERO
 var screensize = Vector2(480, 720)
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 func start():
 	set_process(true)
@@ -37,3 +35,12 @@ func _process(delta):
 
 	
 
+
+
+func _on_area_entered(area):
+	if area.is_in_group("coins"):
+		area.pickup()
+		pickup.emit()
+	if area.is_in_group("obstacle"):
+		hurt.emit()
+		die()
